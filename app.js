6b2a374d091813011262f1d617a38556c3e0c29f
NesterovSerhii <sergei_nesterov8@ukr.net>
2024-03-03 15:16:12 +0200

    sum

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ import {
 const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,       
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use('/api/shops', shopsRouter);
 app.use(handleNotFound);
