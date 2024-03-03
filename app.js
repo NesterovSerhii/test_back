51510ee849adb3879e366807542b499637d0fddb
NesterovSerhii <sergei_nesterov8@ukr.net>
2024-03-03 15:43:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ import {
 const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,       
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://192.168.1.56:3000'],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 app.use(express.json())
 app.use('/api/shops', shopsRouter);
